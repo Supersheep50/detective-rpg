b7b3a4130aae5cc8aed0a5580c6d54882db94038
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2025-03-27 14:37:48 +0000

    Added styled menu and background css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLLOOOOOO
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark text-light min-vh-100 p-4">
+      <div className="container mb-5">
+        <pre className="text-monospace bg-dark text-light p-4 rounded">
+{String.raw`
+  _____       _            _   _                  _               _____                      _       
+ |  __ \     | |          | | (_)                | |             / ____|                    | |      
+ | |  | | ___| |_ ___  ___| |_ ___   _____       | | ___   ___  | (___  _ __ ___   __ _ _ __| |_ ___ 
+ | |  | |/ _ \ __/ _ \/ __| __| \ \ / / _ \  _   | |/ _ \ / _ \  \___ \| '_ \` _ \ / _\` | '__| __/ __|
+ | |__| |  __/ ||  __/ (__| |_| |\ V /  __/ | |__| | (_) |  __/  ____) | | | | | | (_| | |  | |_\__ \\
+ |_____/ \___|\__\___|\___|\__|_| \_/ \___|  \____/ \___/ \___| |_____/|_| |_| |_|\__,_|_|   \__|___/
+`}
+        </pre>
+      </div>
+
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="border border-light rounded p-4 bg-secondary text-light" style={{ minWidth: '250px' }}>
+          <h4 className="text-center mb-4">Main Menu</h4>
+          <div className="d-grid gap-3">
+            <button className="btn btn-outline-light">New Game</button>
+            <button className="btn btn-outline-light">Save Game</button>
+            <button className="btn btn-outline-light">About</button>
+            <button className="btn btn-outline-light">Exit Game</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
